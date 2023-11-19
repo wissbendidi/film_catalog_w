2ee06783e08205137369f8f1aa3c53ffec35dd4b
wissbendidi <wissalbendidi.ing@gmail.com>
2023-11-19 22:47:43 +0100

    searchBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import './App.css';
+import SearchBar from './components/composite/SearchBar';
 import { Test } from './components/composite/Test';
 
 function App() {
   return (
     <>
       <div className="text-black bg-white dark:text-white dark:bg-gray-900 m-10">
-        <Test />
+        <SearchBar/>
+       
+<Test />
+
       </div>
-    </>
+        
+            </>
   );
 }
 

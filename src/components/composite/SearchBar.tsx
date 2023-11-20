57626f5e17f57271b9079dd8e6a0ffa19c16bf8e
wissbendidi <wissalbendidi.ing@gmail.com>
2023-11-20 15:03:52 +0100

    modify searchBar

--- src/components/composite/SearchBar.tsx
+++ src/components/composite/SearchBar.tsx
@@ -2,30 +2,29 @@
 
 export default function SearchComponent() {
     return (
-        <div className="flex items-center">
-            <div className="flex space-x-1">
+        <form className="max-w-sm px-4">
+            <div className="relative">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="absolute top-0 bottom-0 w-6 h-6 my-auto text-gray-400 left-3"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                >
+                    <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                    />
+                </svg>
                 <input
                     type="text"
-                    className="block w-full px-4 py-2 text-purple-700 bg-white border rounded-full focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
-                    placeholder="Search for movie "
+                    placeholder="Search for movie..."
+                    className="w-full py-3 pl-12 pr-4 text-gray-500 border rounded-full outline-none bg-gray-50 focus:bg-white focus:border-indigo-600"
                 />
-                <button className="px-4 text-white bg-purple-600 rounded-full ">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="w-5 h-5"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                        strokeWidth={2}
-                    >
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                        />
-                    </svg>
-                </button>
             </div>
-        </div>
+        </form>
     );
 }
+

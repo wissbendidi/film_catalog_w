c66b0f52b43500071566d5e4a57e332d49a4144e
R-Elias <114922778+R-Elias@users.noreply.github.com>
2023-11-23 10:30:56 +0100

    Fix back arrow icon

--- src/components/composite/Details.tsx
+++ src/components/composite/Details.tsx
@@ -32,10 +32,9 @@ export function Details() {
         }}
       ></div>
       <div className="flex items-left">
-        <button className="relative top-0 left-0 m-4 ml-0 p-2 bg-black text-white rounded-[12px] shadow-lg" onClick={() => navigate('/')}>
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline-block" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="{2}" d="M15 19l-7-7 7-7" />
-          </svg>
+        <button className="flex relative top-0 left-0 m-4 ml-0 p-2 transparent text-white rounded-[12px]" onClick={() => navigate('/')}>
+          {/* Source : iconscout.com */}
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" id="back-arrow"><path fill="none" d="M0 0h24v24H0V0z"></path><path d="M19 11H7.83l4.88-4.88c.39-.39.39-1.03 0-1.42-.39-.39-1.02-.39-1.41 0l-6.59 6.59c-.39.39-.39 1.02 0 1.41l6.59 6.59c.39.39 1.02.39 1.41 0 .39-.39.39-1.02 0-1.41L7.83 13H19c.55 0 1-.45 1-1s-.45-1-1-1z"></path></svg>
           <span className="ml-2">Back</span>
         </button>
       </div>
